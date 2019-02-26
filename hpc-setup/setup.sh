b8d3ef348dd1ff0229551b1f6eae6a7f8c1de6e7
Mikkel Møller Brusen <mikkel_mb@hotmail.com>
2019-02-26 15:42:13 +0100

    hpc shell scripts, run them with "bash file_name.sh"

---
+++ hpc-setup/setup.sh
@@ -0,0 +1,13 @@
+export PYTHONPATH=
+python3 -m venv ~/stdpy3 --copies
+source ~/stdpy3/bin/activate
+
+pip3 install -U pip
+pip3 install -U numpy
+pip3 install -U scipy
+pip3 install -U matplotlib
+pip3 install -U scikit-learn
+pip3 install -U panda
+pip3 install -U torch torchvision
+
+
---
+++ run.sh
@@ -0,0 +1,6 @@
+module load python3
+module load cuda/9.1
+export PYTHONPATH=
+source ~/stdpy3/bin/activate
+
+python3 main.py
