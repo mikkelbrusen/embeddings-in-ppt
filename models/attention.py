4854467eb0d881ee622c6cf7f240e13a892fe944
Mikkel Møller Brusen <Mikkel_mb@hotmail.com>
2019-04-09 12:05:40 +0200

    Refactor attention

---
+++ models/attention.py
@@ -0,0 +1,75 @@
+import math
+import torch
+import sys
+import torch.nn as nn
+import torch.nn.functional as F
+sys.path.insert(0,'..')
+from utils import length_to_mask
+
+class Attention(nn.Module):
+  def __init__(self, in_size, att_size):
+    super(Attention, self).__init__()
+    self.linear_in = nn.Linear(in_size, att_size)
+    self.linear_att = nn.Linear(att_size, 1, bias=False)
+    
+  def forward(self, x_in, seq_lengths):  # x_in.shape: [bs, seq_len, in_size]
+    att_vector = self.linear_in(x_in) # [bs, seq_len, att_size]
+    att_hid_align = torch.tanh(att_vector) # [bs, seq_len, att_size]
+    att_score = self.linear_att(att_hid_align).squeeze(2) # [bs, seq_len]
+    
+    mask = length_to_mask(seq_lengths)
+    alpha = F.softmax(att_score + mask, dim=1) # [bs, seq_len]
+    att = alpha.unsqueeze(2) # [bs, seq_len, 1]
+
+    return torch.sum(x_in * att, dim=1), alpha # [bs, in_size]
+
+
+class MultiStepAttention(nn.Module):
+  def __init__(self, input_size, hidden_size, att_size=256, cell_hid_size=512, num_steps=10, directions=2):
+    super(MultiStepAttention, self).__init__()
+    self.directions = directions
+    self.num_steps = num_steps
+
+    # Attention
+    self.linear_in = nn.Linear(input_size, att_size)
+    self.linear_hid = nn.Linear(cell_hid_size, att_size)
+    self.linear_att = nn.Linear(att_size, 1, bias=False)
+    
+    self.use_projection = hidden_size*self.directions != cell_hid_size
+    if (self.use_projection):
+      self.linear_proj = nn.Linear(hidden_size*self.directions, cell_hid_size)
+      
+    self.lstm_cell = nn.LSTMCell(input_size=input_size, hidden_size=cell_hid_size)
+
+  def forward(self, x_in, hidden, seq_lengths): #x_in: (batch_size, seq_len, hidden_size*directions)
+    h_x, c_x = hidden #h_x: (directions, batch_size, hidden_size)
+
+    # Prepare hidden states for lstm_cell
+    h_x = h_x.permute(1,0,2) # (batch_size, directions, hidden_size)
+    c_x = c_x.permute(1,0,2)
+    h_x = h_x.reshape(h_x.size(0), h_x.size(2)*self.directions) #(batch_size, hidden_size*directions)
+    c_x = c_x.reshape(c_x.size(0), c_x.size(2)*self.directions)
+
+    if (self.use_projection):
+      h_x = self.linear_proj(h_x) # (batch_size, cell_hid_size)
+      c_x = self.linear_proj(c_x) # (batch_size, cell_hid_size)
+
+    for i in range(self.num_steps):
+      # Attention
+      att_vector = self.linear_in(x_in) # [bs, seq_len, att_size]
+      hid_vector = self.linear_hid(h_x) # [bs, att_size]
+      att_vector = att_vector + hid_vector.unsqueeze(dim=1)
+      att_hid_align = torch.tanh(att_vector) # [bs, seq_len, att_size]
+      att_score = self.linear_att(att_hid_align).squeeze(2) # [bs, seq_len]
+      mask = length_to_mask(seq_lengths)
+
+      alpha = F.softmax(att_score + mask, dim=1) # [bs, seq_len]
+      att = alpha.unsqueeze(2) # [bs, seq_len, 1]
+
+      c_vector = torch.sum(x_in * att, dim=1) # (batch_size, hidden_size*2)
+      # /attention
+
+      h_x, c_x = self.lstm_cell(input=c_vector, hx=(h_x,c_x)) # h_x: (batch_size, hidden_size*2)
+
+    # output c_vector rather tan h_x to reduce parameters and overfitting
+    return c_vector, alpha
--- models/model.py
+++ models/model.py
@@ -5,66 +5,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 sys.path.insert(0,'..')
+from models.attention import Attention, MultiStepAttention
 from utils import length_to_mask
-
-class Attention(nn.Module):
-  def __init__(self, in_size, att_size, hid_size=512, is_multi_step=False):
-    super(Attention, self).__init__()
-    self.is_multi_step = is_multi_step
-    self.linear_in = nn.Linear(in_size, att_size)
-
-    if (is_multi_step):
-      self.linear_hid = nn.Linear(hid_size, att_size)
-    self.linear_att = nn.Linear(att_size, 1, bias=False)
-    
-  def forward(self, x_in, hidden, seq_lengths):  # x_in.shape: [bs, seq_len, in_size]
-    att_vector = self.linear_in(x_in) # [bs, seq_len, att_size]
-
-    if (self.is_multi_step):
-      hid_vector = self.linear_hid(hidden) # [bs, att_size]
-      att_vector = att_vector + hid_vector.unsqueeze(dim=1)
-
-    att_hid_align = torch.tanh(att_vector) # [bs, seq_len, att_size]
-   
-    att_score = self.linear_att(att_hid_align).squeeze(2) # [bs, seq_len]
-    
-    mask = length_to_mask(seq_lengths)
-
-    alpha = F.softmax(att_score + mask, dim=1) # [bs, seq_len]
-    att = alpha.unsqueeze(2) # [bs, seq_len, 1]
-
-    return torch.sum(x_in * att, dim=1), alpha # [bs, in_size]
-
-class MultiStepAttention(nn.Module):
-  def __init__(self, input_size, hidden_size, att_size=256, cell_hid_size=512, num_steps=10, directions=2):
-    super(MultiStepAttention, self).__init__()
-    self.directions = directions
-    self.num_steps = num_steps
-    self.attn = Attention(in_size=input_size, hid_size=cell_hid_size, att_size=att_size, is_multi_step=True)
-    self.use_projection = hidden_size*self.directions != cell_hid_size
-    if (self.use_projection):
-      self.linear_proj = nn.Linear(hidden_size*self.directions, cell_hid_size)
-      
-    self.lstm_cell = nn.LSTMCell(input_size=input_size, hidden_size=cell_hid_size)
-
-  def forward(self, x_in, hidden, seq_lengths): #x_in: (batch_size, seq_len, hidden_size*directions)
-    h_x, c_x = hidden #h_x: (directions, batch_size, hidden_size)
-
-    # Prepare hidden states for lstm_cell
-    h_x = h_x.permute(1,0,2) # (batch_size, directions, hidden_size)
-    c_x = c_x.permute(1,0,2)
-    h_x = h_x.reshape(h_x.size(0), h_x.size(2)*self.directions) #(batch_size, hidden_size*directions)
-    c_x = c_x.reshape(c_x.size(0), c_x.size(2)*self.directions)
-    if (self.use_projection):
-      h_x = self.linear_proj(h_x) # (batch_size, cell_hid_size)
-      c_x = self.linear_proj(c_x) # (batch_size, cell_hid_size)
-
-    for i in range(self.num_steps):
-      c_vector, alpha = self.attn(x_in=x_in, hidden=h_x, seq_lengths=seq_lengths) # c_vector: (batch_size, hidden_size*2)
-      h_x, c_x = self.lstm_cell(input=c_vector, hx=(h_x,c_x)) # h_x: (batch_size, hidden_size*2)
-
-    # output c_vector rather tan h_x to reduce parameters and overfitting
-    return c_vector, alpha
 
 
 class ABLSTM(nn.Module):
@@ -84,7 +26,7 @@ class ABLSTM(nn.Module):
     if (is_multi_step):
       self.attn = MultiStepAttention(input_size=n_hid*2, hidden_size=n_hid, att_size=att_size, cell_hid_size=cell_hid_size, num_steps=num_steps, directions=directions)
     else:
-      self.attn = Attention(in_size=n_hid*2, hid_size=cell_hid_size, att_size=att_size, is_multi_step=is_multi_step)
+      self.attn = Attention(in_size=n_hid*2, att_size=att_size)
 
     self.dense = nn.Linear(n_hid*2, n_hid*2)
     self.label = nn.Linear(n_hid*2, n_class)
@@ -133,7 +75,7 @@ class ABLSTM(nn.Module):
     packed_output, (h, c) = self.lstm(pack) #h = (2, batch_size, hidden_size)
     output, _ = nn.utils.rnn.pad_packed_sequence(packed_output, batch_first=True) #(batch_size, seq_len, hidden_size*2)
   
-    attn_output, alpha = self.attn(x_in=output, hidden=(h, c), seq_lengths=seq_lengths) #(batch_size, hidden_size*2) alpha = (batch_size, seq_len)
+    attn_output, alpha = self.attn(x_in=output, seq_lengths=seq_lengths) #(batch_size, hidden_size*2) alpha = (batch_size, seq_len)
     output = self.drop(attn_output)
     
     output = self.relu(self.dense(output)) # (batch_size, hidden_size*2)
@@ -147,14 +89,12 @@ class ABLSTM(nn.Module):
 class SimpleAttention(nn.Module):
   def __init__(self, batch_size, n_hid, n_class, drop_per, att_size=256):
     super(SimpleAttention, self).__init__()
-
-    self.attn = Attention(in_size=n_hid, att_size=att_size, is_multi_step=False)
+    self.attn = Attention(in_size=n_hid, att_size=att_size)
     self.in_drop = nn.Dropout2d(drop_per)
     self.label = nn.Linear(n_hid, n_class)
     self.mem = nn.Linear(n_hid, 1)
  
     self.init_weights()
-    
     
   def init_weights(self):  
     self.label.bias.data.zero_()
@@ -162,7 +102,7 @@ class SimpleAttention(nn.Module):
 
   def forward(self, inp, seq_lengths):
     output = self.in_drop(inp) #(batch_size, seq_len, emb_size)
-    attn_output, alpha = self.attn(x_in=output, hidden=None, seq_lengths=seq_lengths) #(batch_size, hidden_size)
+    attn_output, alpha = self.attn(x_in=output, seq_lengths=seq_lengths) #(batch_size, hidden_size)
     out = self.label(attn_output) #(batch_size, num_classes)
     out_mem = torch.sigmoid(self.mem(attn_output)) #(batch_size, 1)
 
