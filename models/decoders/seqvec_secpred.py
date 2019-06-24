3538ac873de0037b3bf577d50a695f4f59a7a877
Gustav Madslund <Gustav.Madslund@gmail.com>
2019-06-24 14:43:09 +0200

    Added SeqVec for secondary structure prediction

---
+++ models/decoders/seqvec_secpred.py
@@ -0,0 +1,23 @@
+import sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class Decoder(nn.Module):
+
+  def __init__(self, args, in_size):
+    super().__init__()
+    self.args = args
+    
+    self.drop = nn.Dropout(p=0.25)
+    self.relu = nn.ReLU()
+
+    self.cnn_1 = nn.Conv1d(in_channels=in_size, out_channels=32, kernel_size=7, padding= 7//2)
+    self.cnn_2 = nn.Conv1d(in_channels=32, out_channels=8, kernel_size=7, padding= 7//2)
+    
+  def forward(self, inp, seq_lengths):
+    output = self.drop(self.relu(self.cnn_1(inp)))
+    out = self.cnn_2(output).permute(0,2,1)
+  
+    return out
