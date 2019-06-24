52710e5045187d52353e74acf76f77665444044f
Gustav Madslund <Gustav.Madslund@gmail.com>
2019-06-24 17:16:00 +0200

    New configs secpred

---
+++ configs/secpred/elmo_deeploc_last_after.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+
+from configs.secpred.base import Config as BaseConfig
+
+from models.encoders.elmo_deeploc import Encoder
+from models.decoders.lstm_mlp2 import Decoder
+
+class Model(nn.Module):
+  def __init__(self, args):
+    super().__init__()
+    self.args = args
+    self.encoder = Encoder(args=args, elmo_layer="last", architecture="after")
+    self.decoder = Decoder(args=args, in_size=args.n_hid*2+300)
+
+  def forward(self, inp, seq_len):
+    inp = inp.long()
+    output = self.encoder(inp, seq_len)
+    output = self.decoder(output, seq_len)
+    return output
+
+class Config(BaseConfig):
+  def __init__(self, args):
+    super().__init__(args, Model)
